5d74a9379fe098a398f63faff56dbb940697c443
wanglin <wanglin@jiqid.com>
2019-10-29 17:15:41 +0800

    修改AI Write

--- sousou_aiwrite.py
+++ sousou_aiwrite.py
@@ -35,13 +35,13 @@ def write_file(content):
 def main(word):
 	data = [["名称", "价格", "销量", "链接"]]
 	for i in range(page_num):
-		url = 'https://s.taobao.com/search?q=AI+Write&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20191029&ie=utf8'
+		url = 'https://s.taobao.com/search?q=AI+Write&imgfile=&commend=all&ssid=s5-e&search_type=item&sourceId=tb.index&spm=a21bo.2017.201856-taobao-item.1&ie=utf8&initiative_id=tbindexz_20170306'
 		headers = {
 			"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
 			"accept-encoding": "gzip, deflate, br",
 			"accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
 			"cache-control": "max-age=0",
-			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; x5sec=7b227365617263686170703b32223a226665386638316166643735356663373863376530323431346337363864383033434c4c34332b3046454c474f6a346e733661534147526f4c4f446b354f5463334d5449334f7a453d227d; JSESSIONID=D61C06582DF6CB349FE28E39D161B0F8; isg=BPT0IxWhFZD2b4EnOKwSSneNxbKmZRtrfB6JBY5VgH8C-ZRDtt3oR6q7fXGEGlAP; l=dBj4yQHIqRENAQWDBOCanurza77OSIRYYuPzaNbMi_5BR6Tsva_OkaViiF96VjW5TyTB4Rb-zx99-etkZmNDEnK-g3fr738r8eCD4",
+			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; x5sec=7b227365617263686170703b32223a223037343762313539656438363562633834346632313037376535353034663036434d7946344f304645507533354c43707261543946426f4c4f446b354f5463334d5449334f7a553d227d; JSESSIONID=669A01B5E2CD0E32935F8A6644804494; isg=BFJSCRBQW1W6DKddSvKM0OX7oxj0y1Uhrohv3xyrfoXwL_IpBPOmDVhNm8u2RM6V; l=dBj4yQHIqRENASPyBOCanurza77OSIRYYuPzaNbMi_5ZZ6T1idbOkNQaUF96VjW5TDTB4Rb-zx99-etkZwTKJ9--g3fP7Mb7tqYH4",
 			"referer": "https://www.taobao.com/",
 			"upgrade-insecure-requests": "1",
 			"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
