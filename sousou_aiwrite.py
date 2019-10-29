669b29a77e735ea0f51252ef8c69d1dd78fe46a1
wanglin <wanglin@jiqid.com>
2019-10-29 17:00:33 +0800

    修复查询智能手写板相关品牌数据错乱

--- sousou_aiwrite.py
+++ sousou_aiwrite.py
@@ -35,13 +35,13 @@ def write_file(content):
 def main(word):
 	data = [["名称", "价格", "销量", "链接"]]
 	for i in range(page_num):
-		url = 'https://s.taobao.com/search?ie=utf8&initiative_id=staobaoz_20191029&stats_click=search_radio_all%3A1&js=1&imgfile=&q=36%E8%AE%B0%E6%99%BA%E8%83%BD%E6%89%8B%E5%86%99%E6%9C%AC&suggest=0_1&_input_charset=utf-8&wq=36%E8%AE%B0&suggest_query=36%E8%AE%B0&source=suggest'
+		url = 'https://s.taobao.com/search?q=AI+Write&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20191029&ie=utf8'
 		headers = {
 			"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
 			"accept-encoding": "gzip, deflate, br",
 			"accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
 			"cache-control": "max-age=0",
-			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; JSESSIONID=56F7649EE88344FF8E200BF79B4FC55B; l=dBj4yQHIqRENASKCBOCwSuI8Y0bOSIRAguPRwh4yi_5a-6L1GWQOkaVc1Fp6VjWfThYB4Rb-zx99-etkZLy06Pt-g3fPaxDc.; isg=BJqaMBjUkyI0ux910mpUuO3j60B8Yx15tqC3t6QTRi34FzpRjFtutWBl46Mux5Y9",
+			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; x5sec=7b227365617263686170703b32223a226665386638316166643735356663373863376530323431346337363864383033434c4c34332b3046454c474f6a346e733661534147526f4c4f446b354f5463334d5449334f7a453d227d; JSESSIONID=D61C06582DF6CB349FE28E39D161B0F8; isg=BPT0IxWhFZD2b4EnOKwSSneNxbKmZRtrfB6JBY5VgH8C-ZRDtt3oR6q7fXGEGlAP; l=dBj4yQHIqRENAQWDBOCanurza77OSIRYYuPzaNbMi_5BR6Tsva_OkaViiF96VjW5TyTB4Rb-zx99-etkZmNDEnK-g3fr738r8eCD4",
 			"referer": "https://www.taobao.com/",
 			"upgrade-insecure-requests": "1",
 			"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
--- sousou_gaoman.py
+++ sousou_gaoman.py
@@ -35,13 +35,13 @@ def write_file(content):
 def main(word):
 	data = [["名称", "价格", "销量", "链接"]]
 	for i in range(page_num):
-		url = 'https://s.taobao.com/search?ie=utf8&initiative_id=staobaoz_20191029&stats_click=search_radio_all%3A1&js=1&imgfile=&q=36%E8%AE%B0%E6%99%BA%E8%83%BD%E6%89%8B%E5%86%99%E6%9C%AC&suggest=0_1&_input_charset=utf-8&wq=36%E8%AE%B0&suggest_query=36%E8%AE%B0&source=suggest'
+		url = 'https://s.taobao.com/search?q=%E9%AB%98%E6%BC%AB&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20191029&ie=utf8'
 		headers = {
 			"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
 			"accept-encoding": "gzip, deflate, br",
 			"accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
 			"cache-control": "max-age=0",
-			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; JSESSIONID=56F7649EE88344FF8E200BF79B4FC55B; l=dBj4yQHIqRENASKCBOCwSuI8Y0bOSIRAguPRwh4yi_5a-6L1GWQOkaVc1Fp6VjWfThYB4Rb-zx99-etkZLy06Pt-g3fPaxDc.; isg=BJqaMBjUkyI0ux910mpUuO3j60B8Yx15tqC3t6QTRi34FzpRjFtutWBl46Mux5Y9",
+			"cookie": "cna=7JKiEpFHzlMCAd3iIOrTYAwh; t=1e17a93117f164103cc2ac78ef3512b2; tg=0; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; enc=%2B%2FPNvT4UOUk0ZH95RVO%2BhpnLGpENKcWFl36bVhKrsEPMiqVv%2Ff5JT0WS4sZoOwv9h9GufpldD7eX3oVR9e10cw%3D%3D; miid=7882758021902184618; hng=CN%7Czh-CN%7CCNY%7C156; tracknick=%5Cu68A6%5Cu5FC6%5Cu7F8E%5Cu59EC; thw=cn; _uab_collina=156496885215409652475149; UM_distinctid=16c8876887272e-01f2bccbbcc11c-4f4c0a2e-1fa400-16c887688738fe; _cc_=U%2BGCWk%2F7og%3D%3D; v=0; cookie2=1b553df0625a1c56e14892a01aea4610; _tb_token_=731773ee518a3; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; x5sec=7b227365617263686170703b32223a226663326539393763623864313433333235343139643264366233343931616130434b4836332b3046454b3738786476667076713263686f4c4f446b354f5463334d5449334f7a4d3d227d; JSESSIONID=68BABB26B2BE9F52378F6506516D3EBC; l=dBj4yQHIqRENAebkBOCZSuI8Y0b9SIRAguPRwh4yi_5CD6LsQfQOkaVOpFp6cjWfTLYB4Rb-zx99-etkZLy06Pt-g3fPgxDc.; isg=BA8PUxtojrESqYq2F6W5i7BEnqPZnGCK22dCtCEcr36F8C_yKQFSpGPi9mBriDvO",
 			"referer": "https://www.taobao.com/",
 			"upgrade-insecure-requests": "1",
 			"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
